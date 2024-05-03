136cde9884055ddb7bc70d3990b987706c78b657
jnicklr <jnichollaslr@gmail.com>
2024-05-03 19:35:50 -0300

    :tada: Base do Projeto

---
+++ src/main/java/com/jnicklr/springbootsorteio/SpringBootSorteioApplication.java
@@ -0,0 +1,13 @@
+package com.jnicklr.springbootsorteio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSorteioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSorteioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jnicklr/springbootsorteio/SpringBootSorteioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnicklr.springbootsorteio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSorteioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
